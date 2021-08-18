8dbec6721c9604e8e4341ea833c0633b0ddb1686
leshan24 <76029896+leshan24@users.noreply.github.com>
2021-08-18 19:29:12 +0800

    Update testpush.java

--- testGit1/src/testGit1/testpush.java
+++ testGit1/src/testGit1/testpush.java
@@ -4,7 +4,7 @@ public class testpush {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello,git is a good thing");
+		System.out.println("hello,git is a good thing hehe");
 	}
 
 }
