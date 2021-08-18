d4d9cac0bc3938d6c20e5cd62b543416c126d379
leshan24 <824646305@qq.com>
2021-08-18 19:21:32 +0800

    "a push test"

---
+++ testGit1/src/testGit1/testpush.java
@@ -0,0 +1,10 @@
+package testGit1;
+
+public class testpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello,git is a good thing");
+	}
+
+}
